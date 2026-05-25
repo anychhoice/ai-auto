597be793d50d549d6aa343363e1a999382ae0c4d
최예찬 <anychhoice@yechan.dev>
2026-05-25 23:16:36 +0900

    Track score generator automation config

--- scripts/slack-bot.js
+++ scripts/slack-bot.js
@@ -2,9 +2,14 @@
 import { loadConfig, loadDotEnv } from "../src/config.js";
 import { runSlackCommandLoop } from "../src/slack.js";
 
+function configPathFromArgs(argv) {
+  const index = argv.findIndex((arg) => arg === "--config" || arg === "-c");
+  return index === -1 ? undefined : argv[index + 1];
+}
+
 async function main() {
   loadDotEnv();
-  const config = loadConfig();
+  const config = loadConfig(configPathFromArgs(process.argv.slice(2)));
   await runSlackCommandLoop(config);
 }
 
